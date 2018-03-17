04b5782f41958597fd6ed7a996dece717745da54
chaowei.zhchw <chaowei.zhchw@alibaba-inc.com>
2018-03-17 23:53:49 +0800

    bug fix

--- snakeai/model/model.py
+++ snakeai/model/model.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
 import os
-import numpy as np
 
+import tensorflow as tf
 from tensorflow.python.training.adam import AdamOptimizer
 
 
@@ -14,18 +13,14 @@ class Model:
         self.inputs = inputs
         self.network = network
         self.targets = tf.placeholder(tf.float32, shape=(None, self.output_shape[1]))
+        self.step = 0
 
         summary_names = ["actions", "fruits_eaten", "timesteps_survived"]
 
-        summary_variables = [tf.Variable(name=name + "_variable", dtype=tf.int32,
-                                         initial_value=[0] * self.output_shape[1])
-                             for name in summary_names if name == "actions"]
+        self.summary_placeholders = [tf.placeholder(dtype=tf.int32) for _ in range(len(summary_names))]
 
-        summary_variables += [tf.Variable(name=name + "_variable", dtype=tf.int32, initial_value=0) for name in
-                              summary_names if name != "actions"]
-
-        self.summary_placeholders = [tf.placeholder(dtype=summary_variables[i].dtype)
-                                     for i in range(len(summary_names))]
+        # self.summary_placeholders = [tf.placeholder(dtype=summary_variables[i].dtype)
+        #                              for i in range(len(summary_names))]
 
         # summary_ops = [tf.assign(summary_variables[i],self.summary_placeholders[i])
         #                for i in range(len(summary_names))]
@@ -33,15 +28,19 @@ class Model:
         summary_ops = []
 
         summary = [tf.summary.histogram(summary_names[i], self.summary_placeholders[i]) for i in range(1)]
+
         summary += [tf.summary.scalar(summary_names[i], self.summary_placeholders[i]) for i in
                     range(1, len(summary_names))]
 
         self.summary_ops = summary_ops + [tf.summary.merge_all()]
 
-        global_step = tf.train.create_global_step()
         self.loss = tf.losses.mean_squared_error(self.network, self.targets)
         optimizer = AdamOptimizer()
-        self.train_step = optimizer.minimize(loss=self.loss, global_step=global_step)
+        self.train_step = optimizer.minimize(loss=self.loss)
+        #
+        # with tf.colocate_with(global_step):
+        #     self.update_op = tf.assign_add(global_step, 1)
+
         self.sess = tf.Session()
 
         self.summary_writer.add_graph(tf.get_default_graph())
@@ -61,19 +60,23 @@ class Model:
         return tuple(self.network.get_shape().as_list())
 
     def train_on_batch(self, samples, targets, summary):
-        global_step = tf.train.get_global_step()
-        summary[0], _ = np.histogram(summary[0], bins=range(self.output_shape[1] + 1))
+        self.step += 1
+        # summary[0], _ = np.histogram(summary[0], bins=range(self.output_shape[1] + 1))
         # feed_dict = {self.summary_ops[i]: summary[i] for i in range(len(summary))}
         feed_dict = {self.summary_placeholders[i]: summary[i] for i in range(len(summary))}
         feed_dict[self.inputs] = samples
         feed_dict[self.targets] = targets
-        run_result = self.sess.run(fetches=dict(summary=self.summary_ops, loss=self.loss, train_step=self.train_step),
-                                   feed_dict=feed_dict)
-        self.summary_writer.add_summary(run_result["summary"][-1], tf.train.global_step(self.sess, global_step))
+        run_result = self.sess.run(
+            fetches=dict(summary=self.summary_ops, loss=self.loss, train_step=self.train_step),
+            feed_dict=feed_dict)
+        self.summary_writer.add_summary(run_result["summary"][-1], self.step)
         return run_result["loss"]
 
     def predict(self, samples):
         return self.sess.run(fetches=self.network, feed_dict={self.inputs: samples})
+
+    def record_summary(self, summary):
+        pass
 
     def save(self, file_name):
         self.saver.save(self.sess, "/tmp/dqn/" + file_name)
